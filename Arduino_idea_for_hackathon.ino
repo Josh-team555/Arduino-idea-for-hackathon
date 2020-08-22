086add12785e2a5b4cf08f068a51800726ec17e8
kidx9 <69366448+kidx9@users.noreply.github.com>
2020-08-22 17:41:44 -0400

    Version 3 Jokes
    The QUEST is complete!!!!

--- Arduino_idea_for_hackathon.ino
+++ Arduino_idea_for_hackathon.ino
@@ -1,5 +1,10 @@
+String firstKnockKnock[] = {"Brett", "Bacon", "Butter", "Norway", "Alda"};
+String secondKnockKnock[] = {"BrettYouDoNotKnowWhoThisIs", "IAmBaconACakeForYourBirthday", "IButterNotTellYouItIsASecret", "ThereIsNorwayIAmLeavingUntilYouOpenThisDoor", "AldaKidsLikeMyKnockKnockJokes"};
+
+
 void setup() {
   Serial.begin(9600);   //Start the serial monitor
+  
 }
 
 void loop() {
@@ -7,10 +12,34 @@ void loop() {
   
   if (i == 1) {
     //catagory one
-  }
+    Serial.println("knock-knock");
+    while (true) {
+      String stringStorage = Serial.readString(); 
+      if (stringStorage != "") {
+        stringStorage = "";
+        break;
+      }
+    }
+    
+    int j = random(0,6);
+    Serial.println(firstKnockKnock[j]);
+     
+    while (true) {
+      String stringStorage = Serial.readString(); 
+      if (stringStorage != "") {
+        stringStorage = "";
+        break;
+      }
+    }
+  
+    Serial.println(secondKnockKnock[j]);
+    delay(1000);
+  } 
+  
   else if(i == 2) {
     //catagory two
   }
+  
   else {
     //catagory three
   }
