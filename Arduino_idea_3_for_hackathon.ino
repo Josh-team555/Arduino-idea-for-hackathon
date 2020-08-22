6c26ef7599148dfef5e14911be9d98aead372c54
kidx9 <69366448+kidx9@users.noreply.github.com>
2020-08-22 18:02:16 -0400

    The BadLibs draft 1
    BadLibs start

---
+++ Arduino_idea_3_for_hackathon.ino
@@ -0,0 +1,14 @@
+
+
+void setup() {
+  Serial.begin(9600);   //Start the serial monitor
+  Serial.println("Welcome to BadLibs™®®");
+  Serial.println("Enter a BadLibs™®©® into the serial monitor in this format:");
+  Serial.println("There was a [noun], who was very [adjective]. . .");
+  Serial.println("Type '!help' to get a full list of the possible inserts for your BadLibs©®™™©™®™©");
+}
+
+void loop() {
+  //Serial.println(Serial.readString());
+  
+}
