98429f1aa8b4a3b2775e2f943bba9ff4728ad12b
Josh-team555 <55858387+Josh-team555@users.noreply.github.com>
2020-08-22 20:14:42 -0400

    Add files via upload
    Finished!!!

--- Haiku_generator.ino
+++ Haiku_generator.ino
@@ -1,10 +1,30 @@
-String nouns[] = {"Camel ", "Person ", "Book ", "iPhone ", "Elephant "};
-int nounSyllVals[] = {2, 2, 1, 2, 3};
+String nounsL1[] = {"Camel ", "Person ", "Book ", "iPhone ", "Elephant "};
+int nounSyllValsL1[] = {2, 2, 1, 2, 3};
 
-String adjectives[] = {"Purple ", "Gray ", "Hairy ", "Loud ", "Confident "};
-int adjectiveSyllVals[] = {2, 1, 2, 1, 3};
-String verbs[] = {"ran", "swam", "flew", "confounded", "entrenched"};
-int verbSyllVals[] = {1, 1, 1, 3, 2};
+String adjectivesL1[] = {"Purple ", "Gray ", "Hairy ", "Loud ", "Confident "};
+int adjectiveSyllValsL1[] = {2, 1, 2, 1, 3};
+
+String verbsL1[] = {"ran", "swam", "flew", "confounded", "entrenched"};
+int verbSyllValsL1[] = {1, 1, 1, 3, 2};
+
+String nounsL2[] = {"Bumblebee ", "Desk ", "Tea Infuser ", "Laptop ", "Frisbee ", "Calculator ", "Moon"}; 
+int nounSyllValsL2[] = {3, 1, 4, 2, 2, 4, 1};
+
+String adjectivesL2[] = {"Blue ", "Communicated ", "Musical ", "Manotonous ", "Peaceful "};
+int adjectiveSyllValsL2[] = {1, 5, 3, 4, 2};
+
+String verbsL2[] = {"Danced", "Ventured", "Decided", "Written", "Lived"};
+int verbSyllValsL2[] = {1, 2, 3, 2, 1};
+
+String nounsL3[] = {"Tree ", "Sun ", "Soccerball ", "Robot ", "Dancer "};
+int nounSyllValsL3[] = {1, 1, 3, 1, 2};
+
+String adjectivesL3[] = {"Colorful ", "Tall ", "Shallow ", "Boring ", "Sparse "};
+int adjectiveSyllValsL3[] = {3, 1, 2, 2, 1};
+
+String verbsL3[] = {"Hypnotized", "Fell", "Strengthened", "Enjoyed", "Threw"};
+int verbSyllValsL3[] = {3, 1, 2, 2, 1};
+
 
 int firstLineSyllVal = 0;
 int secondLineSyllVal = 0;
@@ -20,49 +40,146 @@ void loop() {
   
   
   while (firstLineSyllVal != 5) {
-    firstLineSyllVal = 0;
+    if (firstLineSyllVal != 5) {
+      firstLineSyllVal = 0;
+    } 
+    randomSeed(analogRead(8));
     int j = random(0, 5);
-    Serial.print(adjectives[j]);
     
-    firstLineSyllVal = firstLineSyllVal + adjectiveSyllVals[j];
+    Serial.print(adjectivesL1[j]);
+    
+    firstLineSyllVal = firstLineSyllVal + adjectiveSyllValsL1[j];
     
     if (firstLineSyllVal == 3) {
       
       for (int i = 0; i < 5; i ++) {
-        if (nounSyllVals[i] == 1) {
-          Serial.print(nouns[i]);
-          firstLineSyllVal = firstLineSyllVal + nounSyllVals[i];
+        if (nounSyllValsL1[i] == 1) {
+          Serial.print(nounsL1[i]);
+          firstLineSyllVal = firstLineSyllVal + nounSyllValsL1[i];
           break;
         }
       }
       
     } else if (firstLineSyllVal == 2) {
       for (int i = 0; i < 5; i ++) {
-        if (nounSyllVals[i] == 1 || nounSyllVals[i] == 2) {
-          Serial.print(nouns[i]);
-          firstLineSyllVal = firstLineSyllVal + nounSyllVals[i];
+        if (nounSyllValsL1[i] == 1 || nounSyllValsL1[i] == 2) {
+          Serial.print(nounsL1[i]);
+          firstLineSyllVal = firstLineSyllVal + nounSyllValsL1[i];
           break;
         }
     }
     
   } else {
     j = random(0,5);
-    Serial.print(nouns[j]);
-    firstLineSyllVal = firstLineSyllVal + nounSyllVals[j];
+    Serial.print(nounsL1[j]);
+    firstLineSyllVal = firstLineSyllVal + nounSyllValsL1[j];
   }
     
     int necesaryVal = 5 - firstLineSyllVal;    
      
 
     for (int i = 0; i < 5; i ++) {
-        if (verbSyllVals[i] == necesaryVal) {
-          Serial.print(verbs[i]);
+        if (verbSyllValsL1[i] == necesaryVal) {
+          Serial.print(verbsL1[i]);
+          firstLineSyllVal = firstLineSyllVal + verbSyllValsL1[i];
           break;
         }
     }   
   
+  delay(1000);
+  Serial.println();
+  }
+
+
+  
+  while(secondLineSyllVal != 7) {
+    
+    secondLineSyllVal = 0;
+    randomSeed(analogRead(7));
+    int j = random(0, 5);
+    Serial.print(adjectivesL2[j]);
+    secondLineSyllVal = secondLineSyllVal + adjectiveSyllValsL2[j];
+    if (secondLineSyllVal == 4) {
+      Serial.println(secondLineSyllVal);
+      for (int i = 0; i < 7; i ++) {
+        if (nounSyllValsL2[i] != 3 || nounSyllValsL2 != 4) {
+          Serial.print(nounsL2[i]);
+          secondLineSyllVal = secondLineSyllVal + nounSyllValsL2[i];
+          break;
+        }
+      }
+      
+    } else {
+      randomSeed(analogRead(10));
+      j = random(0,5);
+      Serial.print(nounsL2[j]);
+      secondLineSyllVal = secondLineSyllVal + nounSyllValsL2[j];
+  }
+  int necesaryVal = 7 - secondLineSyllVal;
+  for (int i = 0; i < 5; i ++) {
+        if (verbSyllValsL2[i] == necesaryVal) {
+          Serial.print(verbsL2[i]);
+          secondLineSyllVal = secondLineSyllVal + verbSyllValsL2[i];
+          break;
+        }
+    }
+    delay(1000);
+    Serial.println();
+  }
+
+
+
+  while (thirdLineSyllVal != 5) {
+    if (thirdLineSyllVal != 5) {
+      thirdLineSyllVal = 0;
+    } 
+    randomSeed(analogRead(12));
+    int j = random(0, 5);
+    
+    Serial.print(adjectivesL3[j]);
+    
+    thirdLineSyllVal = thirdLineSyllVal + adjectiveSyllValsL3[j];
+    
+    if (thirdLineSyllVal == 3) {
+      
+      for (int i = 0; i < 5; i ++) {
+        if (nounSyllValsL3[i] == 1) {
+          Serial.print(nounsL3[i]);
+          thirdLineSyllVal = thirdLineSyllVal + nounSyllValsL3[i];
+          break;
+        }
+      }
+      
+    } else if (thirdLineSyllVal == 2) {
+      for (int i = 0; i < 5; i ++) {
+        if (nounSyllValsL3[i] == 1 || nounSyllValsL3[i] == 2) {
+          Serial.print(nounsL3[i]);
+          thirdLineSyllVal = thirdLineSyllVal + nounSyllValsL3[i];
+          break;
+        }
+    }
+    
+  } else {
+    randomSeed(analogRead(4));
+    j = random(0,5);
+    Serial.print(nounsL3[j]);
+    thirdLineSyllVal = thirdLineSyllVal + nounSyllValsL3[j];
+  }
+    
+    int necesaryVal = 5 - thirdLineSyllVal;    
+     
+
+    for (int i = 0; i < 5; i ++) {
+        if (verbSyllValsL3[i] == necesaryVal) {
+          Serial.print(verbsL3[i]);
+          thirdLineSyllVal = thirdLineSyllVal + verbSyllValsL3[i];
+          break;
+        }
+    }   
   
   delay(1000);
   Serial.println();
   }
+
+  
 }
