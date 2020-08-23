eb2b49fc2c5aeddde875a06bdbf20b28620a9785
kidx9 <69366448+kidx9@users.noreply.github.com>
2020-08-23 11:22:16 -0400

    Development-Jokes

--- Arduino_Hackathon_Idea_V2.ino
+++ Arduino_Hackathon_Idea_V2.ino
@@ -8,9 +8,7 @@ void setup() {
 }
 
 void loop() {
-  int i = random(0,3);  //Determine a random number for the first catagory
   
-  if (i == 1) {
     //catagory one
     Serial.println("knock-knock");
     
@@ -35,14 +33,4 @@ void loop() {
      Serial.println(secondKnockKnock[j]);
      check = 1;
     }
-      
-      
-  } else if(i == 2) {
-    //catagory two
-  }
-  else {
-    //catagory three
-  }
-  
-
 }
